8e3a7fe5dcd162d3b4c9b37070b6a7c0f60851bf
atarashi <116485868+saketh42@users.noreply.github.com>
2024-10-06 16:59:36 +0530

    product creation

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {});
+app.use(express.json())
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({success:false, message: "Please provide allfields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct})
+    } catch (error) {
+        console.error("Error in Create product", error.message);
+        res.status(500).json({ success:"false", message:"Server Error"});
+    }
+});
 
 app.listen(5000, () => {
     connectDB(); 
