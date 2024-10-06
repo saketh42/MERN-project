6945ea169f81836ec687e6e6f7ea21d58ab00a01
atarashi <116485868+saketh42@users.noreply.github.com>
2024-10-06 19:03:06 +0530

    Code clean up and bug fix

---
+++ backend/routes/product.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import Product from '../models/product.model.js';
+import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/product.controller.js';
+
+
+const router = express.Router();
+
+router.get("/", getProducts);
+router.post("/", createProduct);
+router.put("/:id", updateProduct)
+router.delete("/:id", deleteProduct);
+
+
+export default router
