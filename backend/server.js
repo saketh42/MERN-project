d99d39b99e270b5cc2957feb435f305a7d2c6afc
atarashi <116485868+saketh42@users.noreply.github.com>
2024-10-06 17:40:43 +0530

    all product update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
 
@@ -36,6 +37,23 @@ app.post("/api/products", async (req, res) => {
         res.status(500).json({ success:"false", message:"Server Error"});
     }
 });
+
+app.put("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const product = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json( { success: false, message: 'Not found' });
+    }
+
+    try {
+        const updateProduct = await Product.findByIdAndUpdate(id, product, { new:true });
+        res.status(200).json({ success: true, data: updateProduct });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+})
 
 app.delete("/api/products/:id", async (req, res) => {
     const {id} = req.params;
