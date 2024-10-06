4c6da7b5b80d1f85a8cbf79456118bfb6ab61bb9
atarashi <116485868+saketh42@users.noreply.github.com>
2024-10-06 17:09:45 +0530

    product deletion

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,17 @@ app.post("/api/products", async (req, res) => {
     }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted" });
+    } catch (error) {
+        res.status(404).json({ success: false, message: "Product not found"});
+    }
+});
+
 app.listen(5000, () => {
     connectDB(); 
     console.log("Server started at https://localhost:5000");
