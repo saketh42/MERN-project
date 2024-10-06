982163854cbd3c540015e3a8ff9ddfe48de36477
atarashi <116485868+saketh42@users.noreply.github.com>
2024-10-06 17:40:30 +0530

    all prodecut fetching

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json())
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products })
+    } catch (error) {
+        console.log("error in fetching products: ", error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+});
+
 app.post("/api/products", async (req, res) => {
     const product = req.body;
 
@@ -34,6 +44,7 @@ app.delete("/api/products/:id", async (req, res) => {
         await Product.findByIdAndDelete(id);
         res.status(200).json({ success: true, message: "Product deleted" });
     } catch (error) {
+        console.log("error in deleting products: ", error.message);
         res.status(404).json({ success: false, message: "Product not found"});
     }
 });
