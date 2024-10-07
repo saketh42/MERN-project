929328d5a8d0240987daef2f65e151ac58b9c9fc
atarashi <116485868+saketh42@users.noreply.github.com>
2024-10-07 23:22:50 +0530

    initial commit

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -16,7 +16,7 @@ export const createProduct = async (req, res) => {
     const product = req.body;
 
     if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({success:false, message: "Please provide allfields"});
+        return res.status(400).json({success:false, message: "Please provide all fields"});
     }
 
     const newProduct = new Product(product);
@@ -50,11 +50,15 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
     const {id} = req.params;
     
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json( { success: false, message: 'Not found' });
+    }
+
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({ success: true, message: "Product deleted" });
     } catch (error) {
         console.log("error in deleting products: ", error.message);
-        res.status(404).json({ success: false, message: "Product not found"});
+        res.status(500).json({ success: false, message: "Server Error"});
     }
 }
